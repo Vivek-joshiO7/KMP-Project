7a0fbc5b9b0fc2481ecfc3a9d67a8f48e5a4078b
vivek.joshi <vivek.joshi@brucode.com>
2025-11-14 17:50:44 +0530

    random commit

--- composeApp/src/commonMain/kotlin/com/vivek/demo/ui/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/vivek/demo/ui/screens/home/HomeScreen.kt
@@ -1,26 +1,71 @@
 package com.vivek.demo.ui.screens.home
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.vivek.demo.ui.theme.Blue
+import com.vivek.demo.ui.theme.BlueGray
+import com.vivek.demo.ui.theme.BlueLite70
+import com.vivek.demo.ui.theme.Gray
+import com.vivek.demo.ui.theme.White
+import kmp_project.composeapp.generated.resources.Res
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun HomeScreen(navController: NavController, homeViewModel: HomeViewModel) {
-    Column(modifier = Modifier) {
-        Practice()
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        AnnotatedStringFunctionExample()
     }
 }
 
 @Composable
-fun Practice() {
-    Column {
-        BasicText("Hello Home Screen", modifier = Modifier.fillMaxWidth())
+fun AnnotatedStringFunctionExample() {
+    val annotatedString = buildAnnotatedString {
+        blueGradientText("Introducing")
+        append("\n")
+        blueWhiteGradientText("Kotlin Multiplatform")
+        append("\n")
+        blueGradientText("To World")
+    }
+    BasicText(annotatedString)
+}
+
+private fun AnnotatedString.Builder.blueGradientText(text: String) {
+    withStyle(
+        style = SpanStyle(
+            brush = Brush.linearGradient(listOf(Blue,BlueGray)),
+            fontSize = 42.sp,
+            fontWeight = FontWeight.Medium
+        )
+    ) {
+        append(text)
+    }
+}
+
+private fun AnnotatedString.Builder.blueWhiteGradientText(text: String) {
+    withStyle(
+        style = SpanStyle(
+            brush = Brush.linearGradient(listOf(Blue,White,BlueLite70)),
+            fontSize = 48.sp,
+            fontWeight = FontWeight.Bold
+        )
+    ) {
+        append(text)
     }
 }
 
--- composeApp/src/commonMain/kotlin/com/vivek/demo/ui/theme/Color.kt
+++ composeApp/src/commonMain/kotlin/com/vivek/demo/ui/theme/Color.kt
@@ -16,6 +16,9 @@ val md_theme_dark_onBackground = Color(0xFFE3E3DC)
 
 
 // Additional colors can be defined here as needed
+val Blue = Color(0xFF0A20DE)
+val BlueLite70 = Color(0xB20A20DE)
+val White = Color(0xFFFFFFFF)
 val BlueGray = Color(0xFFA0A3BD)
 val TextGray = Color(0xFF4E4B66)
 val Gray = Color(0xFFB0B3B8)
